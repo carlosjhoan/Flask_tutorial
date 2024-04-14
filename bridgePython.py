a5c1819b0e1777e812194e4be7dc1c478b6745a8
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-04-14 12:17:44 -0500

    Aprendiendo a utilizar decorators en Pyhton

--- bridgePython.py
+++ bridgePython.py
@@ -1,3 +1,15 @@
-from BridgePython import Bridge
-bridge = Bridge(api_key='myapikey', secure=True)
-bridge.connect()
+def do_twice(func):
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+    return wrapper_do_twice
+
+    
+
+@do_twice
+def say_whee():
+    print("Wheeeee!")
+
+#say_whee()
+
+do_twice(say_whee())
