e30eef87acaff7631b5ec75afcc2b9612844d29e
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-04-16 00:12:08 -0500

    Probando las primeras líneas de código usando el Framework Flask

--- bridgePython.py
+++ bridgePython.py
@@ -1,3 +1,5 @@
+from flask import Flask
+
 def do_twice(func):
     def wrapper_do_twice(*args, **kwargs):
         func(*args, **kwargs)
@@ -10,6 +12,19 @@ def do_twice(func):
 def say_whee():
     print("Wheeeee!")
 
-#say_whee()
+say_whee()
+#do_twice(say_whee())
 
-do_twice(say_whee())
+
+
+#from datetime import datetime
+
+#print(datetime.now().hour)
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return '<p>Hello, Carlota qué pasó!</p>'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug = True)
