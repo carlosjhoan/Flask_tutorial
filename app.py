adda476be92b53cb80df7cd5c6468bf70586fa7d
carlosjhoan <43355110+carlosjhoan@users.noreply.github.com>
2024-05-11 02:08:19 +0000

    Creando apis

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 #from datetime import datetime
 
 #print(datetime.now().hour)
@@ -51,6 +51,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html', jobs = JOBS, company_name = 'CarlosJhoan')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = True)
